ed40d2e1ad4d49489ffe08150b22937bfb292165
Richard Rzeszutek <richengguy@users.noreply.github.com>
2021-12-31 22:29:57 -0500

    Some build adjustments...

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
 module.exports = function(eleventyConfig) {
+    // Ensure eleventy is aware of SCSS changes.
+    eleventyConfig.addWatchTarget("scss");
+    eleventyConfig.addWatchTarget("_build");
+
     // Ensure that generated assets (i.e. js & css) are copied, along with
     // everything in "src/publications".
     eleventyConfig.addPassthroughCopy({"_build/css": "css"});
