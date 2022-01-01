24b36e37ac99f282a608239c0a530d0d8d171b9c
Richard Rzeszutek <richengguy@users.noreply.github.com>
2022-01-01 14:32:25 -0500

    CNAME and nojekyll now added to build.

--- .eleventy.js
+++ .eleventy.js
@@ -4,8 +4,10 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addWatchTarget("_build");
 
     // Ensure that generated assets (i.e. js & css) are copied, along with
-    // everything in "src/publications".
+    // everything in "src/publications" and some GH Pages configuration files.
     eleventyConfig.addPassthroughCopy({"_build/css": "css"});
+    eleventyConfig.addPassthroughCopy("src/.nojekyll");
+    eleventyConfig.addPassthroughCopy("src/CNAME");
     eleventyConfig.addPassthroughCopy("src/publications");
 
     // Set 'src' at the source folder and have it output to '_site'.
