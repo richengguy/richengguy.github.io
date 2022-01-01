d17a045993a639e7ac0197ae4ea2ad01eb561bfc
Richard Rzeszutek <richengguy@users.noreply.github.com>
2022-01-01 17:56:40 -0500

    Now using orphaned branches and force-pushes.

--- scripts/update-gh-pages.sh
+++ scripts/update-gh-pages.sh
@@ -8,11 +8,15 @@ cd ${DEPLOYMENT_FOLDER}
 # https://www.innoq.com/en/blog/github-actions-automation/
 repo_uri="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
+git init
+
 git config user.name "richengguy"
 git config user.email "richengguy@users.noreply.github.com"
 
-git remote set-url origin "$repo_uri"
+git remote add repo "$repo_uri"
+git fetch --depth=1 repo
+git checkout --orphan ${DEPLOYMENT_BRANCH}
 
 git add .
 git commit -m "Updated on $(date)."
-git push
+git push --force repo ${DEPLOYMENT_BRANCH}
