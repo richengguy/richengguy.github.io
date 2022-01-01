1f584bf05484c37563e0d026a482199515e04b95
richengguy <richengguy@users.noreply.github.com>
2022-01-01 18:06:23 -0500

    Setup GitHub Actions to deploy the site. (#2)

--- scripts/update-gh-pages.sh
+++ scripts/update-gh-pages.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 set -eu
 
-cd gh-pages
+cd ${DEPLOYMENT_FOLDER}
 
 # Need to use the GitHub authentication token to cause a gh-pages build.  More
 # details here;
 # https://www.innoq.com/en/blog/github-actions-automation/
 repo_uri="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
+# First, setup a new repo in the deployment folder.
+git init
+
 git config user.name "richengguy"
 git config user.email "richengguy@users.noreply.github.com"
 
-git remote set-url origin "$repo_uri"
+# Add the main repo (see above) as a remote and do a shallow fetch.
+git remote add repo "$repo_uri"
+git fetch --depth=1 repo
 
+# Create an orphaned branch for the deployment branch.
+git checkout --orphan ${DEPLOYMENT_BRANCH}
+
+# Add all of the files (basically a clean slate) and perform a force push to
+# update the remote branch and overwrite it.
 git add .
 git commit -m "Updated on $(date)."
-git push
+git push --force repo ${DEPLOYMENT_BRANCH}
