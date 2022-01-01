b4094a405c2153105aa920e87f5697c5db6cf9dd
Richard Rzeszutek <richengguy@users.noreply.github.com>
2021-12-31 20:37:06 -0500

    Setting up the "selected publications" page.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,14 @@
 module.exports = function(eleventyConfig) {
-    // Ensure publications are copied as-is.
-    eleventyConfig.addPassthroughCopy("publications");
-}
+    // Ensure that generated assets (i.e. js & css) are copied, along with
+    // everything in "src/publications".
+    eleventyConfig.addPassthroughCopy({"_build/css": "css"});
+    eleventyConfig.addPassthroughCopy("src/publications");
+
+    // Set 'src' at the source folder and have it output to '_site'.
+    return {
+        dir: {
+            input: 'src',
+            output: '_site'
+        }
+    };
+};
