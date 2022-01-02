27b7f5e52f13c6dea1f4fa16e50c8965db977ac5
richengguy <richengguy@users.noreply.github.com>
2022-01-01 22:59:03 -0500

    Updated the landing page appearance. (#3)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-  content: ["./src/**/*.liquid"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./src/**/*.liquid"],
+    theme: {
+        fontFamily: {
+            'sans': ['europa', 'sans-serif']
+        },
+        extend: {
+            fontFamily: {
+                'header': ['canada-type-gibson', 'sans-serif']
+            }
+        },
+    },
+    plugins: [],
 }
